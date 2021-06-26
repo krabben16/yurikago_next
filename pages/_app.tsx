f33cf1492e6d848e4176fd16455abccb18907ae9
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-06-26 15:19:03 +0900

    add: gtag module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import '~/styles/index.css'
+import usePageview from '~/hooks/usePageview'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  usePageview()
   return <Component {...pageProps} />
 }
