53685e1b4a653fe7d115b61d032791565e2d444e
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-06-27 00:13:22 +0900

    add: remark-gfm module

--- lib/markdown/markdownToHtml.ts
+++ lib/markdown/markdownToHtml.ts
@@ -1,12 +1,14 @@
 // https://github.com/vercel/next.js/tree/canary/examples/blog-starter-typescript
 import remark from 'remark'
-import externalLinks from 'remark-external-links'
+import links from 'remark-external-links'
+import gfm from 'remark-gfm'
 import prism from 'remark-prism'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(externalLinks)
+    .use(links)
+    .use(gfm)
     .use(prism)
     .use(html)
     .process(markdown)
