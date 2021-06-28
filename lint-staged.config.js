d00ffd1c995936828ab10dbd89cfef6b529bab47
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-06-28 22:31:29 +0900

    chore: add comment

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 // https://chocoby.com/blog/2021/02/26/husky-v5-lint-staged-prettier
 // https://github.com/okonet/lint-staged#configuration
 
-// '*': ['prettier --write'] だと以下のエラーが発生する。
+// '*': ['prettier --write'] を設定すると以下のエラーが発生した。
 // × prettier --write:
 // [error] No parser could be inferred for file: .husky\.gitignore
 // [error] No parser could be inferred for file: .husky\pre-commit
@@ -12,10 +12,13 @@
 // info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
 // husky - pre-commit hook exited with code 1 (error)
 
-// yarn lint:fix ではエラーが発生しない。
-// yarn.lock は .gitignore に含まれていないのでリントの対象外
+// yarn lint:eslint --fix ではエラーが発生しない。
+// yarn.lock は .gitignore に含まれているのでリントの対象外
 // .husky/ 以下のファイルはなぜエラーにならない？
-// 拡張子がないファイルはリントされない？
+
+// prettierは以下の形式をサポートしている。
+// リント対象をディレクトリ単位でフィルタリングするとき、サポート外の形式のファイルはスキップしている？
+// https://prettier.io/docs/en/index.html
 
 module.exports = {
   '*.{js,jsx,ts,tsx,json,md}': ['eslint --fix', 'prettier --write'],
