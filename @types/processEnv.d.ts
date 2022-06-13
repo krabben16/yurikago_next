dde1090252e0d1b975cfb5424b0a9e36631ffe39
krabben16 <57534374+krabben16@users.noreply.github.com>
2022-06-13 20:29:57 +0900

    remove: NEXT_PUBLIC_ARTICLE_AUTHORを削除

--- @types/processEnv.d.ts
+++ @types/processEnv.d.ts
@@ -3,7 +3,6 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_FRONT_URL: string
     readonly NEXT_PUBLIC_GITHUB_ID: string
     readonly NEXT_PUBLIC_GITHUB_URL: string
-    readonly NEXT_PUBLIC_ARTICLE_AUTHOR: string
     readonly NEXT_PUBLIC_ARTICLE_ORGANIZATION: string
     readonly NEXT_PUBLIC_SITE_NAME: string
     readonly NEXT_PUBLIC_DISQUS_SHORTNAME: string
