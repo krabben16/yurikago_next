4aa6b88a023aa349006418c46dd402c299c7da46
Hiroki Kawaguchi <57534374+krabben16@users.noreply.github.com>
2022-06-13 18:06:08 +0900

    fix: GitHubへのリンクを削除

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -3,17 +3,11 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <div className="py-3 d-flex align-items-center">
-      <a
-        href={process.env.NEXT_PUBLIC_GITHUB_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          className="rounded-circle"
-          width="60"
-          src={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_ID}.png`}
-        />
-      </a>
+      <img
+        className="rounded-circle"
+        width="60"
+        src={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_ID}.png`}
+      />
       <Link href="/">
         <a className="ms-3 text-decoration-none text-reset fs-3">
           {process.env.NEXT_PUBLIC_SITE_NAME}
