8ac3ad4a69070eb91926855fcd9d60aa12d3b784
krabben16 <57534374+krabben16@users.noreply.github.com>
2022-06-29 00:02:43 +0900

    Remove: eslint-plugin-storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 // https://zenn.dev/thiragi/articles/555a644b35ebc1
 // https://buildersbox.corp-sansan.com/entry/2021/05/28/110000
-// https://zenn.dev/minguu42/articles/20211226-nextjs-storybook
 module.exports = {
-  extends: ["plugin:storybook/recommended", 'next', 'prettier'],
+  extends: ['next', 'prettier'],
   rules: {
     'import/order': [
       'warn',
