a6f14c1d2ecaad7f6151dc4bc4ed3d48b7492822
krabben16 <57534374+krabben16@users.noreply.github.com>
2022-06-28 22:29:37 +0900

    Add: eslint-plugin-storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 // https://zenn.dev/thiragi/articles/555a644b35ebc1
 // https://buildersbox.corp-sansan.com/entry/2021/05/28/110000
+// https://zenn.dev/minguu42/articles/20211226-nextjs-storybook
 module.exports = {
-  extends: ['next', 'prettier'],
+  extends: ["plugin:storybook/recommended", 'next', 'prettier'],
   rules: {
     'import/order': [
       'warn',
